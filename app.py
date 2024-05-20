117cce43fd62378bcb0907cd9a0c93b23716ca46
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-05-20 14:40:41 +0700

    Added a way to validate the url

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+from urllib.parse import urlparse
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 
 @app.route('/')
@@ -11,7 +13,19 @@ def home():
 @app.route('/shorten_url', methods=['POST'])
 def shorten_url():
     long_url = request.form['long_url']
-    return render_template('home.html', long_url=long_url)
+    if is_valid_url(long_url):
+        return render_template('home.html', long_url=long_url)
+    else:
+        flash('Invalid URL')
+        return redirect(url_for('home'))
+
+
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
 
 if __name__ == '__main__':
