824955567c389e9591de4ac6a40cb5bcd487ca59
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-05-20 15:25:53 +0700

    Added a login page (Still need the correct validation for login to work)

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 
+#Home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
+#URL shortener
 @app.route('/shorten_url', methods=['POST'])
 def shorten_url():
     long_url = request.form['long_url']
@@ -20,12 +22,30 @@ def shorten_url():
         return redirect(url_for('home'))
 
 
+#Checker for URL
 def is_valid_url(url):
     try:
         result = urlparse(url)
         return all([result.scheme, result.netloc])
     except ValueError:
         return False
+
+
+# Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Need to add the correct saved validation here:
+        if username == 'user' and password == 'pass':
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
