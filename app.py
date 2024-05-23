14eb05fdc9805d277dc446240f59fc2a459deeca
Levan Dalbashvili <levanidalbashvili00@gmail.com>
2024-05-23 16:37:03 +0700

    fixed button

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ url_mapping = {}
 
 def generate_short_url():
     """Generate a random string of 6 characters prefixed with 'fatnik'."""
-    prefix = "fatnik"
+    prefix = "rfefiuhger9uwhfuerhjfioewhjifejfatnik"
     suffix = "".join(random.choices(string.ascii_letters + string.digits, k=6))
     return prefix + suffix
 
